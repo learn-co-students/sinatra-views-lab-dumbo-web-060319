8e79bb6d733be752ad09b87e8c12c932ac290589
Kevin Lai <kevinlai76@gmail.com>
2019-06-24 17:16:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,20 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		@today = Date.today.strftime("%A, %B %d, %Y")
+		@weekday = @today.split(", ")[0]
+		@month_day = @today.split(", ")[1]
+		@year = @today.split(", ")[2]
+		erb :date
+	end
 
 end
