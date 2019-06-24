676d01712ff2f3dac95690e31776b522f142ed17
github username <github email address>
2019-06-24 15:56:34 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
 class App < Sinatra::Base
+	attr_reader :date_formatted
 
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		@name = params[:q]
+		erb :goodbye
+	end
+
+	get '/date' do
+		@date_formatted = Time.now.strftime("The date is %A, %B %d, %Y.")
+		erb :date
+	end
 
 end
